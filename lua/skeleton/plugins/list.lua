3954d25457fd57006dda3b2cd3b6d16c5f59bae3
Bruno Mello <bruno.barros.mello@gmail.com>
2022-07-02 21:32:18 -0300

    wip: cmp

--- lua/skeleton/plugins/list.lua
+++ lua/skeleton/plugins/list.lua
@@ -1,3 +1,15 @@
-local M = {{'wbthomason/packer.nvim'}}
+local M = {
+  { 'wbthomason/packer.nvim' },
+  
+  -- Completion and lsp
+  { 'neovim/nvim-lspconfig' },
+  { 'hrsh7th/nvim-cmp' },
+  { 'hrsh7th/cmp-buffer' },
+  { 'hrsh7th/cmp-path' },
+  { 'hrsh7th/cmp-nvim-lua' },
+  { 'hrsh7th/cmp-nvim-lsp' },
+  { 'saadparwaiz1/cmp_luasnip' },
+  { 'onsails/lspkind.nvim' },
+}
 
 return M
