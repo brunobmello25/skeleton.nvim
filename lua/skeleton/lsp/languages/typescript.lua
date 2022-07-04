1bfef5490b6dfa62132b86d5617a0869d4c0f749
Bruno Mello <bruno.barros.mello@gmail.com>
2022-07-03 23:59:45 -0300

    add eslint document formatting

--- lua/skeleton/lsp/languages/typescript.lua
+++ lua/skeleton/lsp/languages/typescript.lua
@@ -10,6 +10,23 @@ function M.setup()
     end,
     capabilities = require('skeleton.lsp.completion').get_capabilities(),
   }
+
+  lspconfig.eslint.setup {
+    on_attach = function(client)
+      basics.keymaps()
+
+      client.resolved_capabilities.document_formatting = true
+
+      local autogroup_eslint_lsp = vim.api.nvim_create_augroup("eslint_lsp", { clear = true })
+
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        pattern = "*",
+        command = "EslintFixAll",
+        group = autogroup_eslint_lsp,
+      })
+    end,
+    capabilities = require('skeleton.lsp.completion').get_capabilities(),
+  }
 end
 
 return M
