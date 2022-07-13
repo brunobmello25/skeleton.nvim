487b03a3ddde6f4284dad04544e1cd6a2f941b16
Bruno Mello <bruno.barros.mello@gmail.com>
2022-07-13 13:08:33 -0300

    add formatting for golang

--- lua/skeleton/lsp/languages/golang.lua
+++ lua/skeleton/lsp/languages/golang.lua
@@ -7,6 +7,7 @@ function M.setup()
   lspconfig.gopls.setup {
     on_attach = function()
       basics.keymaps()
+      vim.cmd([[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()]])
     end,
     capabilities = require('skeleton.lsp.completion').get_capabilities(),
   }
