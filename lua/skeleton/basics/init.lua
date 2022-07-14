8ee0cc69e0d09a3280eaad1cfdbd32d04346258e
Bruno Mello <bruno.barros.mello@gmail.com>
2022-07-14 13:28:08 -0300

    fix window navigation keymaps

--- lua/skeleton/basics/init.lua
+++ lua/skeleton/basics/init.lua
@@ -1,22 +1,16 @@
 local M = {}
 
 function M.setup()
-    vim.opt.tabstop = 2
-    vim.opt.mouse = 'a'
-    vim.opt.expandtab = true
-    vim.opt.number = true
-    vim.opt.relativenumber = true
-    vim.opt.shiftwidth = 2
-    vim.opt.scrolloff = 8
-    vim.opt.wrap = false
-    vim.opt.ignorecase = true
-    vim.cmd('filetype plugin indent on')
-
-    vim.keymap.set('n', '<leader>q', '<cmd>q<CR>')
-    vim.keymap.set('n', '<leader>H', '<C-w>h')
-    vim.keymap.set('n', '<leader>J', '<C-w>j')
-    vim.keymap.set('n', '<leader>K', '<C-w>k')
-    vim.keymap.set('n', '<leader>L', '<C-w>l')
+  vim.opt.tabstop = 2
+  vim.opt.mouse = 'a'
+  vim.opt.expandtab = true
+  vim.opt.number = true
+  vim.opt.relativenumber = true
+  vim.opt.shiftwidth = 2
+  vim.opt.scrolloff = 8
+  vim.opt.wrap = false
+  vim.opt.ignorecase = true
+  vim.cmd('filetype plugin indent on')
 end
 
 return M
--- lua/skeleton/keymaps/init.lua
+++ lua/skeleton/keymaps/init.lua
@@ -1,46 +1,51 @@
 local M = {}
 
 function M.setup()
-    vim.g.mapleader = ' '
+  vim.g.mapleader = ' '
 
-    -- yank to clipboard
-    vim.api.nvim_set_keymap('n', '<leader>y', '"+y<CR>', {
-        noremap = true
-    })
-    vim.api.nvim_set_keymap('v', '<leader>y', '"+y<CR>', {
-        noremap = true
-    })
+  -- yank to clipboard
+  vim.api.nvim_set_keymap('n', '<leader>y', '"+y<CR>', {
+    noremap = true
+  })
+  vim.api.nvim_set_keymap('v', '<leader>y', '"+y<CR>', {
+    noremap = true
+  })
 
-    -- move lines up and down
-    vim.api.nvim_set_keymap('n', '<A-j>', ':m+<CR>==', {
-        noremap = true
-    })
-    vim.api.nvim_set_keymap('n', '<A-k>', ':m-2<CR>==', {
-        noremap = true
-    })
-    vim.api.nvim_set_keymap('v', '<A-j>', ':m+<CR>gv=gv', {
-        noremap = true
-    })
-    vim.api.nvim_set_keymap('v', '<A-k>', ':m-2<CR>gv=gv', {
-        noremap = true
-    })
+  -- move lines up and down
+  vim.api.nvim_set_keymap('n', '<A-j>', ':m+<CR>==', {
+    noremap = true
+  })
+  vim.api.nvim_set_keymap('n', '<A-k>', ':m-2<CR>==', {
+    noremap = true
+  })
+  vim.api.nvim_set_keymap('v', '<A-j>', ':m+<CR>gv=gv', {
+    noremap = true
+  })
+  vim.api.nvim_set_keymap('v', '<A-k>', ':m-2<CR>gv=gv', {
+    noremap = true
+  })
 
-    -- indent lines with > and < in visual mode
-    vim.api.nvim_set_keymap('v', '<Tab>', '>gv', {
-        noremap = true
-    })
-    vim.api.nvim_set_keymap('v', '<S-Tab>', '<gv', {
-        noremap = true
-    })
+  -- indent lines with > and < in visual mode
+  vim.api.nvim_set_keymap('v', '<Tab>', '>gv', {
+    noremap = true
+  })
+  vim.api.nvim_set_keymap('v', '<S-Tab>', '<gv', {
+    noremap = true
+  })
 
-    -- copy relative path
-    vim.cmd('command! CopyRelPath call setreg(\'+\', expand(\'%\'))')
+  -- copy relative path
+  vim.cmd('command! CopyRelPath call setreg(\'+\', expand(\'%\'))')
 
-    -- open explorer
-    vim.api.nvim_set_keymap('n', '<leader>e', ':Ex <CR>', {
-        noremap = true
-    })
+  -- open explorer
+  vim.api.nvim_set_keymap('n', '<leader>e', ':Ex <CR>', {
+    noremap = true
+  })
 
+  vim.keymap.set('n', '<leader>q', '<cmd>q<CR>')
+  vim.keymap.set('n', '<leader>H', '<C-w>h')
+  vim.keymap.set('n', '<leader>J', '<C-w>j')
+  vim.keymap.set('n', '<leader>K', '<C-w>k')
+  vim.keymap.set('n', '<leader>L', '<C-w>l')
 end
 
 return M
