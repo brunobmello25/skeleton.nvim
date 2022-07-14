7f0d737fe9418522dba2be3cbb6c271c3c532111
Bruno Mello <bruno.barros.mello@gmail.com>
2022-07-14 15:29:29 -0300

    reenable catppuccin telescope integration

--- lua/skeleton/colors/init.lua
+++ lua/skeleton/colors/init.lua
@@ -1,14 +1,14 @@
 local M = {}
 
 function M.setup()
-    require('catppuccin').setup({
-        transparent_background = true,
-        integrations = {
-          telescope = false
-        }
-    })
+  require('catppuccin').setup({
+    transparent_background = true,
+    integrations = {
+      telescope = true
+    }
+  })
 
-    vim.cmd [[colorscheme catppuccin]]
+  vim.cmd [[colorscheme catppuccin]]
 end
 
 return M
