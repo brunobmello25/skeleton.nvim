e4beb8ae2e624662200e9470ed4cfcce78d61144
Bruno Mello <bruno.barros.mello@gmail.com>
2022-07-19 16:06:44 -0300

    add bash

--- lua/skeleton/lsp/init.lua
+++ lua/skeleton/lsp/init.lua
@@ -7,6 +7,7 @@ function M.setup()
   require('skeleton.lsp.languages.lua').setup()
   require('skeleton.lsp.languages.ruby').setup()
   require('skeleton.lsp.languages.typescript').setup()
+  require('skeleton.lsp.languages.bash').setup()
 end
 
 return M
