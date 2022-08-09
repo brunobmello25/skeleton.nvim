8a07292fa098e05223764a3e71f8c82ddcb0a964
Bruno Mello <bruno.barros.mello@gmail.com>
2022-08-09 14:38:51 -0300

    add telescope resume keymap

--- lua/skeleton/finder/init.lua
+++ lua/skeleton/finder/init.lua
@@ -1,10 +1,13 @@
 local M = {}
 
 function M.setup()
-  vim.api.nvim_set_keymap('n', '<leader>ff', "<cmd>lua require('telescope.builtin').find_files({ hidden = true })<CR>", { noremap = true })
+  vim.api.nvim_set_keymap('n', '<leader>ff', "<cmd>lua require('telescope.builtin').find_files({ hidden = true })<CR>",
+    { noremap = true })
   vim.api.nvim_set_keymap('n', '<leader>fg', "<cmd>lua require('telescope.builtin').live_grep()<CR>", { noremap = true })
   vim.api.nvim_set_keymap('n', '<leader>fb', "<cmd>lua require('telescope.builtin').buffers()<CR>", { noremap = true })
   vim.api.nvim_set_keymap('n', '<leader>ft', ':TodoTelescope<CR>', { noremap = true })
+  vim.api.nvim_set_keymap('n', '<leader>fd', ':Telescope diagnostics<CR>', { noremap = true })
+  vim.api.nvim_set_keymap('n', '<leader>fr', ':Telescope resume<CR>', { noremap = true })
 end
 
 return M
