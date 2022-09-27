5500b234faef9e0cec66d5703fd2e529aa476730
Bruno Mello <bruno.barros.mello@gmail.com>
2022-09-27 16:20:43 -0300

    neoclip

--- lua/skeleton/neoclip/init.lua
+++ lua/skeleton/neoclip/init.lua
@@ -2,6 +2,9 @@ local M = {}
 
 function M.setup()
   require('neoclip').setup()
+
+  vim.api.nvim_set_keymap('n', '<leader>fy', ":lua require('telescope').extensions.neoclip.neoclip()<CR>",
+    { noremap = true })
 end
 
 return M
