d8ee433fceefcd40ce77ad134cad3a69b48b1da1
Bruno Mello <bruno.barros.mello@gmail.com>
2022-10-31 13:18:58 -0300

    fix deprecated formatting_sync

--- lua/skeleton/lsp/languages/bash.lua
+++ lua/skeleton/lsp/languages/bash.lua
@@ -7,7 +7,7 @@ function M.setup()
   lspconfig.bashls.setup {
     on_attach = function()
       keymaps.setup()
-      vim.cmd([[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()]])
+      vim.cmd([[autocmd BufWritePre <buffer> lua vim.lsp.buf.format()]])
     end,
     capabilities = require('skeleton.lsp.completion').get_capabilities(),
   }
