c2224d58d1c74d011de5cb605b0d5a4e089fc67a
Bruno Mello <bruno.barros.mello@gmail.com>
2022-11-24 12:06:59 -0300

    neogit diffview integration

--- lua/skeleton/git/neogit.lua
+++ lua/skeleton/git/neogit.lua
@@ -1,7 +1,12 @@
 local M = {}
 
 function M.setup()
-  require('neogit').setup()
+  require('neogit').setup({
+    disable_commit_confirmation = true,
+    integrations = {
+      diffview = true,
+    },
+  })
 end
 
 return M
