1d5ac9bf77cdeaca1e6bde5b20d38f942c91ba4b
Bruno Mello <bruno.barros.mello@gmail.com>
2023-01-21 14:03:46 -0300

    feat: ensure installed lsp servers

--- lua/skeleton/lsp/mason.lua
+++ lua/skeleton/lsp/mason.lua
@@ -1,8 +1,19 @@
 local M = {}
 
 function M.setup()
-    require('mason').setup()
-    require('mason-lspconfig').setup()
+  require('mason').setup()
+  require('mason-lspconfig').setup({
+    ensure_installed = {
+      'jsonls',
+      'tailwindcss',
+      'sumneko_lua',
+      'eslint',
+      'gopls',
+      'bashls',
+      'prismals',
+      'jedi_language_server'
+    },
+  })
 end
 
 return M
