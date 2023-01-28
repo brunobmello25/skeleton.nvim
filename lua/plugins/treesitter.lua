d3921fa9e15495b515ed9bbd4ba7ff05de31ac69
Bruno Mello <bruno.barros.mello@gmail.com>
2023-01-28 13:55:16 -0300

    feat: treesitter ensure_installed typescript

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
     build = ':TSUpdate',
     config = function()
       require('nvim-treesitter.configs').setup({
-        ensure_installed = { "c", "lua", "vim", "help" },
+        ensure_installed = { "c", "lua", "vim", "help", "javascript", "typescript", "tsx" },
         -- ensure_installed = 'all',
         highlight = {
           enable = true,
