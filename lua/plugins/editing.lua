545318937c568ce9226e9f529760f9d4dfb862db
Bruno Mello <bruno.barros.mello@gmail.com>
2023-01-28 13:55:52 -0300

    fix: nvim-comment dependencies

--- lua/plugins/editing.lua
+++ lua/plugins/editing.lua
@@ -8,6 +8,7 @@ return {
 
   {
     'terrortylor/nvim-comment',
+    dependencies = { 'JoosepAlviste/nvim-ts-context-commentstring' },
     config = function()
       require('nvim_comment').setup({
         operator_mapping = "<leader>/",
