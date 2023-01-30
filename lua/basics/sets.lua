aaa406a3df4d850e8cbb02b3ed98ef06056142e6
Bruno Mello <bruno.barros.mello@gmail.com>
2023-01-30 19:23:37 -0300

    feat: update sets

--- lua/basics/sets.lua
+++ lua/basics/sets.lua
@@ -1,17 +1,36 @@
 return {
   setup = function()
-    vim.opt.tabstop = 2
-    vim.opt.mouse = 'a'
-    vim.opt.expandtab = true
     vim.opt.number = true
     vim.opt.relativenumber = true
+
+    vim.opt.tabstop = 2
+    vim.opt.softtabstop = 2
     vim.opt.shiftwidth = 2
-    vim.opt.scrolloff = 8
+    vim.opt.expandtab = true
+    vim.opt.smartindent = true
+
     vim.opt.wrap = false
+
+    vim.opt.swapfile = false
+    vim.opt.backup = false
+    vim.opt.undodir = os.getenv("HOME") .. '/.vim/undodir'
+    vim.opt.undofile = true
+
+    vim.opt.hlsearch = false
+    vim.opt.incsearch = true
+
+    vim.opt.termguicolors = true
+
+    vim.opt.mouse = 'a'
+
+    vim.opt.scrolloff = 8
+
     vim.opt.ignorecase = true
+
     vim.cmd('filetype plugin indent on')
     vim.cmd('set splitbelow splitright')
-    vim.opt.incsearch = true
+
+    vim.opt.updatetime = 50
 
     -- code folding
     vim.opt.foldenable = false
