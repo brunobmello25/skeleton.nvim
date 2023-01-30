c93a7910ad80da3ac40c42177ab60a12c9600176
Bruno Mello <bruno.barros.mello@gmail.com>
2023-01-30 19:35:57 -0300

    feat: more remaps

--- lua/basics/keymaps.lua
+++ lua/basics/keymaps.lua
@@ -14,5 +14,9 @@ return {
     -- move selected lines up and down with J and K
     vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
     vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
+
+    vim.keymap.set('n', 'J', 'mzJ`z')
+
+    vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
   end
 }
