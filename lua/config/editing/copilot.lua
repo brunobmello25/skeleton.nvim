fc3bf20178a022795961feb36ea1704c318ce6a6
Bruno Mello <bruno.barros.mello@gmail.com>
2023-01-30 18:57:09 -0300

    fix: copilot

--- lua/config/editing/copilot.lua
+++ lua/config/editing/copilot.lua
@@ -1,7 +1,11 @@
 return {
   init = function()
+    vim.g.copilot_assume_mapped = true
     vim.g.copilot_no_tab_map = true
 
-    vim.keymap.set('i', '<C-u>', 'copilot#Accept("<CR>")')
+    vim.api.nvim_set_keymap("i", "<C-u>", 'copilot#Accept("<CR>")', {
+      silent = true,
+      expr = true
+    })
   end
 }
