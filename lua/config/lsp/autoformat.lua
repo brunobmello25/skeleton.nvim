1a15b1c5f2750282da014014d53ab5c1c1cea10c
Bruno Mello <bruno.barros.mello@gmail.com>
2023-02-01 18:02:00 -0300

    refactor: lsp formatting autocommand

--- lua/config/lsp/autoformat.lua
+++ lua/config/lsp/autoformat.lua
@@ -1,18 +1,20 @@
+local function create_group_and_command(props)
+  local group = vim.api.nvim_create_augroup(props.name, { clear = true })
+
+  vim.api.nvim_create_autocmd("BufWritePre", { command = props.command, group = group })
+end
+
 return {
   setup = function(client, _)
     local name = client.name
     local skip_format_autocmd = name == 'eslint' or name == 'vtsls' or name == 'tsserver'
 
     if not skip_format_autocmd then
-      local group = vim.api.nvim_create_augroup("LspFormatOnSave", { clear = true })
-
-      vim.api.nvim_create_autocmd("BufWritePre", { command = "LspZeroFormat", group = group })
+      create_group_and_command({ name = "LspFormatOnSave", command = "LspZeroFormat" })
     end
 
     if name == 'eslint' then
-      local group = vim.api.nvim_create_augroup('EslintFormatOnSave', { clear = true })
-
-      vim.api.nvim_create_autocmd('BufWritePre', { command = 'EslintFixAll', group = group })
+      create_group_and_command({ name = "EslintFormatOnSave", command = "EslintFixAll" })
     end
   end
 }
