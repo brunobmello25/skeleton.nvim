96635cc5b3a7770c6fbd7d8d92646f908521c045
Bruno Mello <bruno.barros.mello@gmail.com>
2023-02-07 15:24:30 -0300

    fix: lsp format autocommand for current buffer only

--- lua/config/lsp/autoformat.lua
+++ lua/config/lsp/autoformat.lua
@@ -1,20 +1,21 @@
 local function create_group_and_command(props)
   local group = vim.api.nvim_create_augroup(props.name, { clear = true })
 
-  vim.api.nvim_create_autocmd("BufWritePre", { command = props.command, group = group })
+  vim.api.nvim_create_autocmd("BufWritePre", { command = props.command, group = group, buffer = props.bufnr })
 end
 
 return {
-  setup = function(client, _)
-    local name = client.name
-    local skip_format_autocmd = name == 'eslint' or name == 'vtsls' or name == 'tsserver'
+    setup = function(client, bufnr)
+      local name = client.name
+      print("Setting up autoformat for " .. name)
+      local skip_format_autocmd = name == 'eslint' or name == 'vtsls' or name == 'tsserver'
 
-    if not skip_format_autocmd then
-      create_group_and_command({ name = "LspFormatOnSave", command = "LspZeroFormat" })
-    end
+      if not skip_format_autocmd then
+        create_group_and_command({ name = "LspFormatOnSave", command = "LspZeroFormat", bufnr = bufnr })
+      end
 
-    if name == 'eslint' then
-      create_group_and_command({ name = "EslintFormatOnSave", command = "EslintFixAll" })
+      if name == 'eslint' then
+        create_group_and_command({ name = "EslintFormatOnSave", command = "EslintFixAll", bufnr = bufnr })
+      end
     end
-  end
 }
