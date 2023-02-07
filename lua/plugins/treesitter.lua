3efff4c80ec317aa5f99b811a8bae1ebf3d22fb9
Bruno Mello <bruno.barros.mello@gmail.com>
2023-02-07 11:26:40 -0300

    feat: treesitter playground

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,5 +3,10 @@ return {
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
     config = require('config.editing.treesitter').config
+  },
+
+  {
+    'nvim-treesitter/playground',
+    dependencies = { 'nvim-treesitter/nvim-treesitter' }
   }
 }
