c10f1fa71920760848fca4f90bfe27b75b65e1d1
Bruno Mello <bruno.barros.mello@gmail.com>
2023-02-08 15:49:39 -0300

    fix: remove autoformat group

--- lua/config/lsp/autoformat.lua
+++ lua/config/lsp/autoformat.lua
@@ -1,7 +1,5 @@
-local function create_group_and_command(props)
-  local group = vim.api.nvim_create_augroup(props.name, { clear = true })
-
-  vim.api.nvim_create_autocmd("BufWritePre", { command = props.command, group = group, buffer = props.bufnr })
+local function create_commmand(props)
+  vim.api.nvim_create_autocmd("BufWritePre", { command = props.command, buffer = props.bufnr })
 end
 
 return {
@@ -10,11 +8,11 @@ return {
     local skip_format_autocmd = name == 'eslint' or name == 'vtsls' or name == 'tsserver'
 
     if not skip_format_autocmd then
-      create_group_and_command({ name = "LspFormatOnSave", command = "LspZeroFormat", bufnr = bufnr })
+      create_commmand({ command = "LspZeroFormat", bufnr = bufnr })
     end
 
     if name == 'eslint' then
-      create_group_and_command({ name = "EslintFormatOnSave", command = "EslintFixAll", bufnr = bufnr })
+      create_commmand({ command = "EslintFixAll", bufnr = bufnr })
     end
   end
 }
