15015892d8f0ff816b6d26a2655525c048c66288
Bruno Mello <bruno.barros.mello@gmail.com>
2023-02-22 16:02:59 -0300

    refactor: change copilot status helper to use module

--- lua/utils/copilot_status.lua
+++ lua/utils/copilot_status.lua
@@ -1,31 +1,34 @@
-local M = { init = false }
+local M = {}
 
-local status = ''
-local setup = function()
+M._init = false
+M._status = ''
+
+M._setup = function()
+  M._init = true
+
   local api = require('copilot.api')
   local lualine = require('lualine')
 
   api.register_status_notification_handler(function(data)
     -- customize your message however you want
     if data.status == 'Normal' then
-      status = 'Idle'
+      M._status = 'Idle'
     elseif data.status == 'InProgress' then
-      status = 'Loading'
+      M._status = 'Loading'
     else
-      status = data.status or 'Offline' -- might never actually be nil but just in case
+      M._status = data.status or 'Offline' -- might never actually be nil but just in case
     end
-    status = 'Copilot: ' .. status
+    M._status = 'Copilot: ' .. M._status
 
     lualine.refresh()
   end)
 end
 
 M.get_status = function()
-  if not M.init then
-    setup()
-    M.init = true
+  if not M._init then
+    M._setup()
   end
-  return status
+  return M._status
 end
 
 return M
