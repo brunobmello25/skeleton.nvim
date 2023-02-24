7ff36291e590d479788f3f2ab9e49f8b09cbfd3f
Bruno Mello <bruno.barros.mello@gmail.com>
2023-02-24 17:03:04 -0300

    feat: add noice.nvim

--- lua/skeleton/plugins/ui.lua
+++ lua/skeleton/plugins/ui.lua
@@ -16,4 +16,17 @@ return {
   --   'NvChad/nvim-colorizer.lua',
   --   config = require('skeleton.config.ui.colorizer').config
   -- },
+
+  {
+    "folke/noice.nvim",
+    config = require('skeleton.config.ui.noice').config,
+    dependencies = {
+      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+      "MunifTanjim/nui.nvim",
+      -- OPTIONAL:
+      --   `nvim-notify` is only needed, if you want to use the notification view.
+      --   If not available, we use `mini` as the fallback
+      "rcarriga/nvim-notify",
+    }
+  }
 }
