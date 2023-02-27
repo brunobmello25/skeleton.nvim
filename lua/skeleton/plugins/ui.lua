cee10ee08054c231e1e1760923b2ed9a9ef63d88
Bruno Mello <bruno.barros.mello@gmail.com>
2023-02-27 12:22:56 -0300

    feat: remove noice

--- lua/skeleton/plugins/ui.lua
+++ lua/skeleton/plugins/ui.lua
@@ -11,22 +11,4 @@ return {
     dependencies = { { 'kyazdani42/nvim-web-devicons' } },
     config = require('skeleton.config.ui.dashboard').config
   },
-
-  -- {
-  --   'NvChad/nvim-colorizer.lua',
-  --   config = require('skeleton.config.ui.colorizer').config
-  -- },
-
-  {
-    "folke/noice.nvim",
-    config = require('skeleton.config.ui.noice').config,
-    dependencies = {
-      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-      "MunifTanjim/nui.nvim",
-      -- OPTIONAL:
-      --   `nvim-notify` is only needed, if you want to use the notification view.
-      --   If not available, we use `mini` as the fallback
-      "rcarriga/nvim-notify",
-    }
-  }
 }
