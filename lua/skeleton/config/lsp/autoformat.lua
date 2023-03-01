2c8a4cabf2644acd93edef8095f3cb08202b9577
Bruno Mello <bruno.barros.mello@gmail.com>
2023-03-01 13:41:29 -0300

    fix: remove double formatting autocommands for typescript with eslint files

--- lua/skeleton/config/lsp/autoformat.lua
+++ lua/skeleton/config/lsp/autoformat.lua
@@ -2,18 +2,41 @@ local function create_commmand(props)
   vim.api.nvim_create_autocmd("BufWritePre", { command = props.command, buffer = props.bufnr })
 end
 
+local function eslint_config_exists()
+  -- search for eslint files: .eslintrc.js, .eslintrc.json, .eslintrc.yaml, .eslintrc.yml, .eslintrc
+  local eslint_files = vim.fn.glob('.eslintrc*')
+  return eslint_files ~= ''
+end
+
 local M = {}
 
 function M.setup(client, bufnr)
-  local name = client.name
-  local skip_format_autocmd = name == 'eslint' or name == 'vtsls' or name == 'tsserver'
+  -- local name = client.name
+  -- local skip_format_autocmd = name == 'eslint' or name == 'vtsls' or name == 'tsserver' or name == 'tailwindcss'
+  --
+  -- if name == 'eslint' then
+  --   create_commmand({ command = "EslintFixAll", bufnr = bufnr })
+  -- elseif not skip_format_autocmd then
+  --   create_commmand({ command = "LspZeroFormat", bufnr = bufnr })
+  -- end
 
-  if not skip_format_autocmd then
-    create_commmand({ command = "LspZeroFormat", bufnr = bufnr })
+  local name = client.name
+
+  if name == 'tailwindcss' then
+    return
   end
 
-  if name == 'eslint' then
+  local is_eslint = name == 'eslint'
+  local is_vtsls = name == 'vtsls'
+
+  if is_eslint and eslint_config_exists() then
     create_commmand({ command = "EslintFixAll", bufnr = bufnr })
+    return
+  end
+
+  if is_vtsls and not eslint_config_exists() then
+    create_commmand({ command = "LspZeroFormat", bufnr = bufnr })
+    return
   end
 end
 
