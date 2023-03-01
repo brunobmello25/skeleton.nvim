4d34fcc3e83ba3f57b9274b62ec52035017a27be
Bruno Mello <bruno.barros.mello@gmail.com>
2023-03-01 20:32:04 -0300

    refactor: lsp format with eslint and others

--- lua/skeleton/config/lsp/autoformat.lua
+++ lua/skeleton/config/lsp/autoformat.lua
@@ -3,7 +3,6 @@ local function create_commmand(props)
 end
 
 local function eslint_config_exists()
-  -- search for eslint files: .eslintrc.js, .eslintrc.json, .eslintrc.yaml, .eslintrc.yml, .eslintrc
   local eslint_files = vim.fn.glob('.eslintrc*')
   return eslint_files ~= ''
 end
@@ -11,15 +10,6 @@ end
 local M = {}
 
 function M.setup(client, bufnr)
-  -- local name = client.name
-  -- local skip_format_autocmd = name == 'eslint' or name == 'vtsls' or name == 'tsserver' or name == 'tailwindcss'
-  --
-  -- if name == 'eslint' then
-  --   create_commmand({ command = "EslintFixAll", bufnr = bufnr })
-  -- elseif not skip_format_autocmd then
-  --   create_commmand({ command = "LspZeroFormat", bufnr = bufnr })
-  -- end
-
   local name = client.name
 
   if name == 'tailwindcss' then
@@ -29,17 +19,19 @@ function M.setup(client, bufnr)
   local is_eslint = name == 'eslint'
   local is_vtsls = name == 'vtsls'
 
-  if is_eslint and eslint_config_exists() then
+  if is_eslint and not eslint_config_exists() then
+    return
+  end
+
+  if is_vtsls and eslint_config_exists() then
+    return
+  end
+
+  if is_eslint then
     create_commmand({ command = "EslintFixAll", bufnr = bufnr })
-    return
-  end
-
-  if is_vtsls and not eslint_config_exists() then
+  else
     create_commmand({ command = "LspZeroFormat", bufnr = bufnr })
-    return
   end
-
-  create_commmand({ command = "LspZeroFormat", bufnr = bufnr })
 end
 
 return M
