9152b759c30cdf533dd6cb35a82202ba203f7d76
Bruno Mello <bruno.barros.mello@gmail.com>
2023-07-10 14:00:50 -0300

    dap adjustments

--- lua/skeleton/plugins/dap.lua
+++ lua/skeleton/plugins/dap.lua
@@ -64,29 +64,26 @@ return {
       'mfussenegger/nvim-dap',
       'rcarriga/nvim-dap-ui',
     },
-    -- ft = { 'python' },
     config = function()
       local dap = require('dap')
       local dap_python = require('dap-python')
 
-      local path = vim.fn.getcwd() .. '/venv/bin/python'
+      local path
+      local venv_path = vim.fn.getcwd() .. '/venv/bin/python'
+      local mason_path = require("mason-registry").get_package("debugpy"):get_install_path() .. '/venv/bin/python'
+
+      if vim.fn.filereadable(venv_path) == 1 then
+        path = venv_path
+      elseif vim.fn.filereadable(mason_path) then
+        path = mason_path
+      elseif vim.fn.exepath('python') ~= '' then
+        path = vim.fn.exepath('python')
+      elseif vim.fn.exepath('python3') ~= '' then
+        path = vim.fn.exepath('python3')
+      else
+        path = 'python'
+      end
       dap_python.setup(path)
-
-
-      -- table.insert(dap.configurations.python, {
-      --   name = "Launch Flask server",
-      --   type = "python",
-      --   request = "launch",
-      --   console = "integratedTerminal",
-      --   cwd = "${workspaceFolder}",
-      --   program = path,
-      --   args = {
-      --     "-m",
-      --     "flask",
-      --     "run",
-      --     "--debug",
-      --   },
-      -- })
     end
   },
 
