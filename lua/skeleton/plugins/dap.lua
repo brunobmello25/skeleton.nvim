fda69dec22795bc21e592a75fee218213c3e1843
Bruno Mello <bruno.barros.mello@gmail.com>
2023-07-10 13:26:10 -0300

    python debugging working

--- lua/skeleton/plugins/dap.lua
+++ lua/skeleton/plugins/dap.lua
@@ -6,6 +6,14 @@ return {
       vim.keymap.set('n', '<F11>', '<cmd>lua require"dap".step_into()<CR>')
       vim.keymap.set('n', '<F12>', '<cmd>lua require"dap".step_out()<CR>')
       vim.keymap.set('n', '<F5>', '<cmd>lua require"dap".continue()<CR>')
+      vim.keymap.set('n', '<F5>', function()
+        if vim.fn.filereadable(".vscode/launch.json") then
+          require("dap.ext.vscode").load_launchjs()
+          print('setup vscode')
+        end
+
+        require('dap').continue()
+      end)
       vim.keymap.set('n', '<leader>db', '<cmd>lua require"dap".toggle_breakpoint()<CR>')
       vim.keymap.set('n', '<leader>dc',
         '<cmd>lua require"dap".set_breakpoint(vim.fn.input("Breakpoint condition: "))<CR>')
@@ -57,38 +65,29 @@ return {
       'mfussenegger/nvim-dap',
       'rcarriga/nvim-dap-ui',
     },
-    ft = { 'python' },
+    -- ft = { 'python' },
     config = function()
       local dap = require('dap')
       local dap_python = require('dap-python')
 
-      local venv_path = vim.fn.getcwd() .. '/venv/bin/python'
-      local mason_path = require('mason-registry').get_package('debugpy'):get_install_path() .. '/venv/bin/python'
-
-      local path = venv_path
-      if vim.fn.filereadable(path) == 0 then
-        path = mason_path
-      end
-      if vim.fn.filereadable(path) == 0 then
-        path = vim.fn.exepath('python')
-      end
-
+      local path = vim.fn.getcwd() .. '/venv/bin/python'
       dap_python.setup(path)
 
-      table.insert(dap.configurations.python, {
-        name = "Launch Flask server",
-        type = "python",
-        request = "launch",
-        console = "integratedTerminal",
-        cwd = "${workspaceFolder}",
-        program = path,
-        args = {
-          "run",
-          "flask",
-          "--no-debug",
-          "run",
-        },
-      })
+
+      -- table.insert(dap.configurations.python, {
+      --   name = "Launch Flask server",
+      --   type = "python",
+      --   request = "launch",
+      --   console = "integratedTerminal",
+      --   cwd = "${workspaceFolder}",
+      --   program = path,
+      --   args = {
+      --     "-m",
+      --     "flask",
+      --     "run",
+      --     "--debug",
+      --   },
+      -- })
     end
   }
 }
