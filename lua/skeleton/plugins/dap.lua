70b601e15761f14705af51c66d4efd4413428da5
Bruno Mello <bruno.barros.mello@gmail.com>
2023-07-10 13:45:03 -0300

    nvim dap virtual text

--- lua/skeleton/plugins/dap.lua
+++ lua/skeleton/plugins/dap.lua
@@ -88,5 +88,16 @@ return {
       --   },
       -- })
     end
+  },
+
+  {
+    'theHamsta/nvim-dap-virtual-text',
+    dependencies = {
+      'mfussenegger/nvim-dap',
+      'nvim-treesitter/nvim-treesitter'
+    },
+    config = function()
+      require("nvim-dap-virtual-text").setup()
+    end
   }
 }
