470f5caf07a9d9454b32e7c9b3b72a79a6bb7b31
Bruno Mello <bruno.barros.mello@gmail.com>
2023-07-18 21:53:29 -0300

    blame toggle command alias

--- lua/skeleton/plugins/git_blame.lua
+++ lua/skeleton/plugins/git_blame.lua
@@ -5,5 +5,9 @@ return {
       vim.g.gitblame_ignored_filetypes = { 'netrw' }
       vim.g.gitblame_enabled = 0
     end,
+    config = function()
+      vim.api.nvim_create_user_command('GBT', 'GitBlameToggle', {
+      })
+    end
   },
 }
