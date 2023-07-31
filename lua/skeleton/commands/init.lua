1d980a90a7b30dc414701e2d7aaf0fc134670059
Bruno Mello <bruno.barros.mello@gmail.com>
2023-07-31 14:02:37 -0300

    CurrentBufferNumber command

--- lua/skeleton/commands/init.lua
+++ lua/skeleton/commands/init.lua
@@ -3,6 +3,11 @@ local M = {}
 function M.setup()
   require('skeleton.commands.splits').setup()
   require('skeleton.commands.cargowatch').setup()
+
+  vim.api.nvim_create_user_command("CurrentBufferNumber", function()
+    print(vim.api.nvim_get_current_buf())
+  end, {
+  })
 end
 
 return M
