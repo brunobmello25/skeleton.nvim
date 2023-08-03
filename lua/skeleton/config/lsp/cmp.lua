05e944a8dab8692ebe6e9bb02495538479181bd3
Bruno Mello <bruno.barros.mello@gmail.com>
2023-08-03 15:36:47 -0300

    remove cmp tab

--- lua/skeleton/config/lsp/cmp.lua
+++ lua/skeleton/config/lsp/cmp.lua
@@ -24,28 +24,6 @@ function M.setup()
       ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
       ['<CR>'] = cmp.mapping.confirm({ select = true }),
       ['<C-u>'] = vim.NIL,
-      ["<Tab>"] = cmp.mapping(function(fallback)
-        if cmp.visible() then
-          cmp.select_next_item()
-          -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
-          -- they way you will only jump inside the snippet region
-        elseif luasnip.expand_or_jumpable() then
-          luasnip.expand_or_jump()
-        elseif has_words_before() then
-          cmp.complete()
-        else
-          fallback()
-        end
-      end, { "i", "s" }),
-      ["<S-Tab>"] = cmp.mapping(function(fallback)
-        if cmp.visible() then
-          cmp.select_prev_item()
-        elseif luasnip.jumpable(-1) then
-          luasnip.jump(-1)
-        else
-          fallback()
-        end
-      end, { "i", "s" }),
     }
   })
 end
