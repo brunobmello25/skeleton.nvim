7672abfd3880ba2b6db2ba620f6ffcdf0dc43894
Bruno Mello <bruno.barros.mello@gmail.com>
2023-08-03 15:37:13 -0300

    config codelldb for rust tools

--- lua/skeleton/config/lsp/init.lua
+++ lua/skeleton/config/lsp/init.lua
@@ -28,18 +28,26 @@ function M.config()
       'jsonls',
       'bashls',
       'tailwindcss',
+      'pylsp',
     }
   })
 
   local rust_tools = require('rust-tools')
 
+  local codelldb_path = vim.fn.exepath('codelldb')
+  local liblldb_path = require("mason-registry").get_package("codelldb"):get_install_path() ..
+      '/extension/lldb/lib/liblldb.so'
+
   rust_tools.setup({
+    dap = {
+      adapter = require('rust-tools.dap').get_codelldb_adapter(codelldb_path, liblldb_path)
+    },
     server = {
       on_attach = function(client, bufnr)
         require('skeleton.config.lsp.keymaps').setup(client, bufnr)
 
         -- Hover actions
-        vim.keymap.set("n", "<F5>", rust_tools.hover_actions.hover_actions, { buffer = bufnr })
+        vim.keymap.set("n", "<leader><leader>.", rust_tools.hover_actions.hover_actions, { buffer = bufnr })
         -- Code action groups
         vim.keymap.set("n", "<Leader>.", rust_tools.code_action_group.code_action_group, { buffer = bufnr })
       end
