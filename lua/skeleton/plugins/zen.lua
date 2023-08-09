3b15065400cc7cf0c02a306e7cb0c8a5af6ec0a4
Bruno Mello <bruno.barros.mello@gmail.com>
2023-08-09 17:03:43 -0300

    change zenmode width

--- lua/skeleton/plugins/zen.lua
+++ lua/skeleton/plugins/zen.lua
@@ -1,16 +1,11 @@
 return {
   {
     "folke/zen-mode.nvim",
-    opts = {
-      -- your configuration comes here
-      -- or leave it empty to use the default settings
-      -- refer to the configuration section below
-    },
     config = function()
       vim.keymap.set('n', '<leader>o', function()
         require("zen-mode").toggle({
           window = {
-            width = .70 -- width will be 85% of the editor width
+            width = .85
           }
         })
       end)
