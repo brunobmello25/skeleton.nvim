b33621352fa4f40c1b6add154adaeca2584b3adb
Bruno Mello <bruno.barros.mello@gmail.com>
2023-08-17 10:10:59 -0300

    disable lazygit

--- lua/skeleton/plugins/lazygit.lua
+++ lua/skeleton/plugins/lazygit.lua
@@ -1,8 +1,9 @@
 return {
-  {
-    'kdheepak/lazygit.nvim',
-    config = function()
-      vim.keymap.set('n', '<leader>lg', '<cmd>LazyGit<cr>')
-    end
-  }
+  -- TODO: delete this once I'm used to fugitive
+  -- {
+  --   'kdheepak/lazygit.nvim',
+  --   config = function()
+  --     vim.keymap.set('n', '<leader>lg', '<cmd>LazyGit<cr>')
+  --   end
+  -- }
 }
