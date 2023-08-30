ec874b925113d45535853436852c4f8feade8909
Bruno Mello <bruno.barros.mello@gmail.com>
2023-08-30 10:25:12 -0300

    chatgpt plugin config

---
+++ lua/skeleton/plugins/chatgpt.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "jackMort/ChatGPT.nvim",
+    event = "VeryLazy",
+    config = function()
+      require("chatgpt").setup({
+        api_key_cmd = vim.fn.stdpath("config") .. "/get_gpt_api_key.sh",
+      })
+    end,
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim"
+    }
+  }
+}
