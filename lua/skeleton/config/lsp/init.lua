cd087825750d68e7316063bb4af3fac2e3fbcfab
Bruno Mello <bruno.barros.mello@gmail.com>
2023-09-22 12:14:20 -0300

    add conform and remove formatting logic from lsp

--- lua/skeleton/config/lsp/init.lua
+++ lua/skeleton/config/lsp/init.lua
@@ -1,54 +1,53 @@
 local M = {}
 
 function M.config()
-  vim.opt.signcolumn = 'yes'
+  vim.opt.signcolumn = "yes"
 
-  local lsp = require('lsp-zero').preset({})
+  local lsp = require("lsp-zero").preset({})
 
   lsp.on_attach(function(client, bufnr)
-    require('skeleton.config.lsp.keymaps').setup(client, bufnr)
-    require('skeleton.config.lsp.autoformat').setup(client, bufnr)
+    require("skeleton.config.lsp.keymaps").setup(client, bufnr)
   end)
 
-  lsp.skip_server_setup({ 'rust_analyzer' })
+  lsp.skip_server_setup({ "rust_analyzer" })
 
   lsp.setup()
 
-  require('skeleton.config.lsp.cmp').setup()
+  require("skeleton.config.lsp.cmp").setup()
 
-  require('mason-lspconfig').setup({
+  require("mason-lspconfig").setup({
     ensure_installed = {
-      'eslint',
-      'lua_ls',
-      'vtsls',
-      'jsonls',
-      'bashls',
-      'tailwindcss',
-      'pylsp',
-      'rust_analyzer',
-    }
+      "eslint",
+      "lua_ls",
+      "vtsls",
+      "jsonls",
+      "bashls",
+      "tailwindcss",
+      "pylsp",
+      "rust_analyzer",
+    },
   })
 
-  local rust_tools = require('rust-tools')
+  local rust_tools = require("rust-tools")
 
-  local codelldb_path = vim.fn.exepath('codelldb')
-  local liblldb_path = require("mason-registry").get_package("codelldb"):get_install_path() ..
-      '/extension/lldb/lib/liblldb.so'
+  local codelldb_path = vim.fn.exepath("codelldb")
+  local liblldb_path = require("mason-registry").get_package("codelldb"):get_install_path()
+    .. "/extension/lldb/lib/liblldb.so"
 
   rust_tools.setup({
     dap = {
-      adapter = require('rust-tools.dap').get_codelldb_adapter(codelldb_path, liblldb_path)
+      adapter = require("rust-tools.dap").get_codelldb_adapter(codelldb_path, liblldb_path),
     },
     server = {
       on_attach = function(client, bufnr)
-        require('skeleton.config.lsp.keymaps').setup(client, bufnr)
+        require("skeleton.config.lsp.keymaps").setup(client, bufnr)
 
         -- Hover actions
         vim.keymap.set("n", "<leader><leader>.", rust_tools.hover_actions.hover_actions, { buffer = bufnr })
         -- Code action groups
         vim.keymap.set("n", "<Leader>.", rust_tools.code_action_group.code_action_group, { buffer = bufnr })
-      end
-    }
+      end,
+    },
   })
 end
 
