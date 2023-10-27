392caff8dbd163a4947614ed6d453905c4f82e36
Bruno Mello <bruno.barros.mello@gmail.com>
2023-10-27 10:44:29 -0300

    update plugins

--- lua/skeleton/plugins/git_blame.lua
+++ lua/skeleton/plugins/git_blame.lua
@@ -1,13 +1,13 @@
 return {
   {
-    'f-person/git-blame.nvim',
-    init = function()
-      vim.g.gitblame_ignored_filetypes = { 'netrw' }
-      vim.g.gitblame_enabled = 0
-    end,
+    "f-person/git-blame.nvim",
     config = function()
-      vim.api.nvim_create_user_command('GBT', 'GitBlameToggle', {
+      require("gitblame").setup({
+        enabled = false,
+        ignored_filetypes = { "netrw" },
       })
-    end
+
+      vim.api.nvim_create_user_command("GBT", "GitBlameToggle", {})
+    end,
   },
 }
