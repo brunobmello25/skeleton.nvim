4ccce33bfcb1853b547e4be11ae13bf66c787581
Bruno Mello <bruno.barros.mello@gmail.com>
2023-11-12 18:33:55 -0300

    markdown preview

---
+++ lua/skeleton/plugins/markdown_preview.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    build = function()
+      vim.fn["mkdp#util#install"]()
+    end,
+  },
+}
