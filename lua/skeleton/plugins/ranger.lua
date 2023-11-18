8c678c8bfa7773584f9d1fd2043ecd41eaf4f1b2
Bruno Mello <bruno.barros.mello@gmail.com>
2023-11-18 15:45:56 -0300

    change from rnvimr to ranger

--- lua/skeleton/plugins/ranger.lua
+++ lua/skeleton/plugins/ranger.lua
@@ -1,11 +1,23 @@
 return {
+  -- {
+  --   'kevinhwang91/rnvimr',
+  --   init = function()
+  --     vim.g.rnvimr_enable_picker = 1
+  --   end,
+  --   config = function()
+  --     vim.keymap.set('n', '<leader>e', '<cmd>RnvimrToggle<cr>')
+  --   end
+  -- },
   {
-    'kevinhwang91/rnvimr',
+    "francoiscabrol/ranger.vim",
+    dependencies = {
+      "rbgrouleff/bclose.vim",
+    },
     init = function()
-      vim.g.rnvimr_enable_picker = 1
+      vim.g.ranger_map_keys = 0
     end,
     config = function()
-      vim.keymap.set('n', '<leader>e', '<cmd>RnvimrToggle<cr>')
-    end
+      vim.keymap.set("n", "<leader>e", "<cmd>Ranger<cr>")
+    end,
   },
 }
