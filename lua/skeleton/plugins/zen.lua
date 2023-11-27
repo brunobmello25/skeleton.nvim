872cb40071e12cdad89109f72c70fe501d58e1e1
Bruno Mello <bruno.barros.mello@gmail.com>
2023-11-27 17:06:37 -0300

    feat: increase zenmode window size

--- lua/skeleton/plugins/zen.lua
+++ lua/skeleton/plugins/zen.lua
@@ -2,13 +2,13 @@ return {
   {
     "folke/zen-mode.nvim",
     config = function()
-      vim.keymap.set('n', '<leader>o', function()
+      vim.keymap.set("n", "<leader>o", function()
         require("zen-mode").toggle({
           window = {
-            width = .85
-          }
+            width = 1,
+          },
         })
       end)
-    end
-  }
+    end,
+  },
 }
