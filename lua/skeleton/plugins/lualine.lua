f3ed06c7ec89ef798aa7a92b982acedcb9c2b900
Bruno Mello <bruno.barros.mello@gmail.com>
2023-11-29 17:52:29 -0300

    global status lualine

--- lua/skeleton/plugins/lualine.lua
+++ lua/skeleton/plugins/lualine.lua
@@ -1,20 +1,20 @@
 return {
   {
-    'nvim-lualine/lualine.nvim',
-    dependencies = { 'kyazdani42/nvim-web-devicons', lazy = true },
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "kyazdani42/nvim-web-devicons", lazy = true },
     config = function()
-      require('lualine').setup({
+      require("lualine").setup({
         sections = {
-          lualine_c = { { 'filename', path = 1 } }
+          lualine_c = { { "filename", path = 1 } },
         },
         options = {
-          theme = 'catppuccin',
+          theme = "catppuccin",
           icons_enabled = true,
-          globalstatus = false,
+          globalstatus = true,
           disabled_filetypes = { "alpha", "dashboard", "NvimTree", "Outline" },
           always_divide_middle = true,
         },
       })
-    end
+    end,
   },
 }
