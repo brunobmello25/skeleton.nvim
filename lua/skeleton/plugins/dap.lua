970d46332c18e89a7e3b3d546e25bb4078f57cff
Bruno Mello <bruno.barros.mello@gmail.com>
2023-12-03 13:05:47 -0300

    typescript debugger configuration

--- lua/skeleton/plugins/dap.lua
+++ lua/skeleton/plugins/dap.lua
@@ -2,6 +2,9 @@ return {
   {
     "mfussenegger/nvim-dap",
     config = function()
+      local dap = require("dap")
+      local mason_registry = require("mason-registry")
+
       vim.keymap.set("n", "<F10>", '<cmd>lua require"dap".step_into()<CR>')
       vim.keymap.set("n", "<F11>", '<cmd>lua require"dap".step_over()<CR>')
       vim.keymap.set("n", "<F12>", '<cmd>lua require"dap".step_out()<CR>')
@@ -27,6 +30,31 @@ return {
         require("dap").disconnect()
         require("dapui").close()
       end, {})
+
+      local node_debug_path = mason_registry.get_package("node-debug2-adapter"):get_install_path()
+        .. "/out/src/nodeDebug.js"
+
+      dap.adapters.node2 = {
+        type = "executable",
+        command = "node",
+        args = {
+          node_debug_path,
+        },
+      }
+
+      dap.configurations.typescript = {
+        {
+          name = "attach to typescript",
+          type = "node2",
+          request = "attach",
+          port = function()
+            return tonumber(vim.fn.input("Debug Port: ", "9229"))
+          end,
+          cwd = vim.fn.getcwd(),
+          sourceMaps = true,
+          skipFiles = { "<node_internals>/**", "node_modules/**" },
+        },
+      }
     end,
   },
 
