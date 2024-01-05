79f294496c675cf53b42c12778fea5f8bcd4535e
Bruno Mello <bruno.barros.mello@gmail.com>
2024-01-05 12:50:11 -0300

    update oil config

--- lua/skeleton/plugins/oil.lua
+++ lua/skeleton/plugins/oil.lua
@@ -1,3 +1,7 @@
+local M = {}
+
+M.show_node_modules = true
+
 return {
   {
     "stevearc/oil.nvim",
@@ -8,11 +12,34 @@ return {
           ["."] = "actions.parent",
           ["q"] = "actions.close",
         },
+        view_options = {
+          -- Show files and directories that start with "."
+          show_hidden = true,
+          -- This function defines what is considered a "hidden" file
+          -- is_hidden_file = function(name, bufnr)
+          --   return vim.startswith(name, ".")
+          -- end,
+          -- This function defines what will never be shown, even when `show_hidden` is set
+          -- second argument is bufnr
+          is_always_hidden = function(name, _)
+            return name == "node_modules" and not M.show_node_modules
+          end,
+          sort = {
+            -- sort order can be "asc" or "desc"
+            -- see :help oil-columns to see which columns are sortable
+            { "type", "asc" },
+            { "name", "asc" },
+          },
+        },
       })
 
+      -- create a user command "ShowNodeModules"
+      -- vim.api.nvim_create_user_command("ToggleNodeModules", function()
+      --   M.show_node_modules = not M.show_node_modules
+      -- end, {})
+
       vim.keymap.set("n", "<leader>e", function()
-        local bufnr = require("oil").toggle_float()
-        print(bufnr)
+        require("oil").toggle_float()
       end)
     end,
   },
