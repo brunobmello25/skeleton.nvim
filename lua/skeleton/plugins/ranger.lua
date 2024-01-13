0c682fb8eeee7d014adbc47cdabf967014019549
Bruno Mello <bruno.barros.mello@gmail.com>
2024-01-13 17:36:02 -0300

    rollback ranger

---
+++ lua/skeleton/plugins/ranger.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "francoiscabrol/ranger.vim",
+    dependencies = {
+      "rbgrouleff/bclose.vim",
+    },
+    init = function()
+      vim.g.ranger_map_keys = 0
+    end,
+    config = function()
+      vim.keymap.set("n", "<leader>e", "<cmd>Ranger<cr>")
+    end,
+  },
+}
