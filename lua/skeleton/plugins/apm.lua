d281d9039e12be9b62c8ef7afc4c13837d8f0c70
Bruno Mello <bruno.barros.mello@gmail.com>
2024-02-09 18:07:26 -0300

    add vim apm

---
+++ lua/skeleton/plugins/apm.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "ThePrimeagen/vim-apm",
+    config = function()
+      local apm = require("vim-apm")
+
+      apm:setup({})
+      vim.keymap.set("n", "<leader>apm", function() apm:toggle_monitor() end)
+    end
+  }
+}
