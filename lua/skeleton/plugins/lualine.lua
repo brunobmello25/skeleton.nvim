461e63cf2c33fdc57683bb5f10f2b7076da18222
Bruno Mello <bruno.barros.mello@gmail.com>
2024-02-16 15:59:02 -0300

    add lualine debugging icon

--- lua/skeleton/plugins/lualine.lua
+++ lua/skeleton/plugins/lualine.lua
@@ -5,7 +5,23 @@ return {
     config = function()
       require("lualine").setup({
         sections = {
-          lualine_c = { { "filename", path = 1 } },
+          lualine_c = {
+            { "filename", path = 1 },
+            {
+              function()
+                return " "
+                -- return require("dap").status()
+              end,
+              icon = { "", color = { fg = "#e7c664" } }, -- nerd icon.
+              cond = function()
+                if not package.loaded.dap then
+                  return false
+                end
+                local session = require("dap").session()
+                return session ~= nil
+              end,
+            },
+          },
         },
         options = {
           theme = "catppuccin",
