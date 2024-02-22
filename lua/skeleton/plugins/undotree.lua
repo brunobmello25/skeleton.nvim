71107504fe8ec381ad407c2071d90dcdcf06d619
Bruno Mello <bruno.barros.mello@gmail.com>
2024-02-22 17:32:04 -0300

    change undotree keymap

--- lua/skeleton/plugins/undotree.lua
+++ lua/skeleton/plugins/undotree.lua
@@ -2,7 +2,7 @@ return {
   {
     "mbbill/undotree",
     config = function()
-      vim.keymap.set("n", "<leader>ut", vim.cmd.UndotreeToggle)
+      vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
     end
   }
 }
