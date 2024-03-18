24960f63bf44f72020eed27adf6dc29a1f18f18e
Bruno Mello <bruno.barros.mello@gmail.com>
2024-03-18 18:12:50 -0300

    fugitive diffget keymaps

--- lua/skeleton/plugins/fugitive.lua
+++ lua/skeleton/plugins/fugitive.lua
@@ -2,7 +2,8 @@ return {
   {
     'tpope/vim-fugitive',
     config = function()
-      vim.keymap.set('n', '<leader>gcs', "<cmd>Gvdiffsplit!<CR>", { desc = "Open fugitive conflict resolution" })
+      vim.keymap.set("n", "gf", "<cmd>diffget //2<CR>", { desc = "Get left side of diff" })
+      vim.keymap.set("n", "gj", "<cmd>diffget //3<CR>", { desc = "Get left side of diff" })
     end
   },
 }
