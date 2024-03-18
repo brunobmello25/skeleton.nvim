ca8eacb22381ef6fb2ccb77258a69813135e9238
Bruno Mello <bruno.barros.mello@gmail.com>
2024-03-18 15:36:53 -0300

    fugitive git conflict solve

--- lua/skeleton/plugins/fugitive.lua
+++ lua/skeleton/plugins/fugitive.lua
@@ -1,5 +1,8 @@
 return {
   {
-    'tpope/vim-fugitive'
+    'tpope/vim-fugitive',
+    config = function()
+      vim.keymap.set('n', '<leader>gcs', "<cmd>Gvdiffsplit!<CR>", { desc = "Open fugitive conflict resolution" })
+    end
   },
 }
