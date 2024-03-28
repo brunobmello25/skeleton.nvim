a559e9a3549b488823e3d43e48250e6c8b43ff0a
Bruno Mello <bruno.barros.mello@gmail.com>
2024-03-28 11:24:51 -0300

    add neotest

---
+++ lua/skeleton/plugins/neotest.lua
@@ -0,0 +1,36 @@
+local function setup_neotest_go()
+  -- get neotest namespace (api call creates or returns namespace)
+  local neotest_ns = vim.api.nvim_create_namespace("neotest")
+  vim.diagnostic.config({
+    virtual_text = {
+      format = function(diagnostic)
+        local message =
+            diagnostic.message:gsub("\n", " "):gsub("\t", " "):gsub("%s+", " "):gsub("^%s+", "")
+        return message
+      end,
+    },
+  }, neotest_ns)
+end
+
+return {
+  {
+    "nvim-neotest/neotest",
+    dependencies = {
+      "nvim-neotest/nvim-nio",
+      "nvim-lua/plenary.nvim",
+      "antoinemadec/FixCursorHold.nvim",
+      "nvim-treesitter/nvim-treesitter",
+      -- adapters:
+      "nvim-neotest/neotest-go"
+    },
+    config = function()
+      setup_neotest_go()
+
+      require('neotest').setup({
+        adapters = {
+          require('neotest-go')
+        },
+      })
+    end
+  }
+}
