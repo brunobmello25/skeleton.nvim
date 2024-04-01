38763cea42e2dc6690cf881ba3deb00d9cce0720
Bruno Mello <bruno.barros.mello@gmail.com>
2024-04-01 11:59:10 -0300

    ensure installed pyright

--- lua/skeleton/config/lsp/init.lua
+++ lua/skeleton/config/lsp/init.lua
@@ -30,8 +30,8 @@ function M.config()
       "jsonls",
       "bashls",
       "tailwindcss",
-      "pylsp",
       "rust_analyzer",
+      "pyright"
     },
     handlers = {
       lsp.default_setup,
