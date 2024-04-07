c9cd2a4d6899fe684cf57ff6449c1f55ff4b09b3
Bruno Mello <bruno.barros.mello@gmail.com>
2024-04-06 22:29:18 -0300

    change markdown plugin

---
+++ lua/skeleton/plugins/markdown.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    'MeanderingProgrammer/markdown.nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter' },
+    config = function()
+      require('render-markdown').setup({})
+    end,
+  }
+}
