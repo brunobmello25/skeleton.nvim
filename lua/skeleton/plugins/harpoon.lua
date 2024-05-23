830caa5155c3a4982fc0bc1f2e8aea0ffed5f6c0
Bruno Mello <bruno.barros.mello@gmail.com>
2024-05-23 13:20:42 -0300

    update harpoon deprecated method

--- lua/skeleton/plugins/harpoon.lua
+++ lua/skeleton/plugins/harpoon.lua
@@ -13,7 +13,7 @@ return {
       })
 
       vim.keymap.set("n", "<leader>ha", function()
-        harpoon:list():append()
+        harpoon:list():add()
       end, { desc = "Add current file to harpoon" })
 
       vim.keymap.set("n", "<leader>hh", function()
